8a4b3797d55b32dd6269c0978842a9fe01ce579b
therealchrisrock <chris@tilde.technology>
2025-10-03 03:26:06 -0400

    feat: create dto abstraction on concrete repo

--- src/backend/shared/infrastructure/persistence/__init__.py
+++ src/backend/shared/infrastructure/persistence/__init__.py
@@ -2,10 +2,12 @@
 
 from .base_mapper import BaseMapper
 from .base_model import BaseModel
+from .sqlalchemy_repository import SQLAlchemyRepository
 from .unit_of_work import SQLAlchemyUnitOfWork
 
 __all__ = [
     "BaseMapper",
     "BaseModel",
+    "SQLAlchemyRepository",
     "SQLAlchemyUnitOfWork",
 ]
